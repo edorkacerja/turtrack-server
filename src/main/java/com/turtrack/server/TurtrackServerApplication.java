9aad97295ab5eaff90be97d914fe306f060e400e
Edor Kacerja <edorkacerja@gmail.com>
2024-11-22 20:30:18 -0700

    try 34

--- src/main/java/com/turtrack/server/TurtrackServerApplication.java
+++ src/main/java/com/turtrack/server/TurtrackServerApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TurtrackServerApplication {
-	static {
-		System.setProperty("spring.profiles.active", "prod");
-	}
+//	static {
+//		System.setProperty("spring.profiles.active", "prod");
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TurtrackServerApplication.class, args);
