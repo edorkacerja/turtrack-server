4ffd4b1e21b60f9c2da31b931996f671d9ed327a
Edor Kacerja <edorkacerja@gmail.com>
2024-11-23 17:11:34 -0700

    added api/v1 to auth controller

--- src/main/java/com/turtrack/server/controller/AuthController.java
+++ src/main/java/com/turtrack/server/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
