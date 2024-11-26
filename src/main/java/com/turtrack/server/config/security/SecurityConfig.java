7dbb0ffe3d9b8172a9f0fb0e443f4afd78a9b799
Edor Kacerja <edorkacerja@gmail.com>
2024-11-25 17:37:19 -0700

    added prices for cors

--- src/main/java/com/turtrack/server/config/security/SecurityConfig.java
+++ src/main/java/com/turtrack/server/config/security/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                                 "/api/v1/subscriptions/plans",
                                 "/api/v1/products/**",
                                 "/api/v1/products",
+                                "/api/v1/prices/**",
                                 "/api/v1/webhook"
                         ).permitAll()
                         .anyRequest().authenticated()
